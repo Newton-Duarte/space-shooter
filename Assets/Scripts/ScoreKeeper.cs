b5d694d63205a7f1b99c9dc9c262ef3f21f84e1e
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-10-07 06:58:34 -0300

    feat: add singleton pattern on score keeper

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -1,8 +1,29 @@
+using System;
 using UnityEngine;
 
 public class ScoreKeeper : MonoBehaviour
 {
     int score;
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        int instanceCount = FindObjectsByType<ScoreKeeper>(FindObjectsSortMode.None).Length;
+
+        if (instanceCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
     public int GetScore()
     {
